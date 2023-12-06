b2af65939a21ed42be9a0aa7a60b2f1c5bb3ac9f
Pashupatibansode <138433004+Pashupatibansode@users.noreply.github.com>
2023-12-06 13:16:49 +0000

    Learning

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+"use strict";    //Treat all js code as newer version
+
+// alert(3+3) // we are using node js not browser
+
+// Data-types (primitive datatypes)
+
+//  1 Number-----   it represent number d.t only
+
+//  2 bigInt-----   in bigInt we can use large No. like stock market no. like this
+
+//  3 String-----   "it can be used in double quote or single quote prefer double quote num&text can be written"
+
+//  4 Boolean-----   it takes only true or false value e.g. user is loged? yes or no => true or false
+
+//  5 null--------   Stand alone value in js null is not undefined   null is objects
+
+//  6 undefined----  Value is not assigned
+
+//  7 Symbol-----    To find unique componant
+
+// --------------Objects-------
+
+
